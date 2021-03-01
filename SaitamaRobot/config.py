826dc24a6a7d6d4b836ebc0414fe668b729e1419
emaheshvar <78479795+emaheshvar@users.noreply.github.com>
2021-03-01 10:17:59 +0530

    Update and rename sample_config.py to config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    API_ID =2206512 # integer value, dont use ""
+    API_HASH = "ddde413169a2466690d18fc3530f5adc"
+    TOKEN = "1698160689:AAEE7_OJkDdkrOA6ukLW9oTkc5GhcHLTy9E"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1288940468  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "BeybladeMaster29"
+    SUPPORT_CHAT = "@wbbaSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001176052381
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001176052381
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
@@ -34,7 +34,7 @@ class Config(object):
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "5DsROdJeFmOCHCkvNrFVve8LdYV0H57jDI208UlwKHUx~TX3PUIPSygsgGj9mpEA"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
@@ -46,25 +46,25 @@ class Config(object):
     DEMONS = get_user_list("elevated_users.json", "supports")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     TIGERS = get_user_list("elevated_users.json", "tigers")
-    WOLVES = get_user_list("elevated_users.json", "whitelists")
+    WOLVES = 1408783553("elevated_users.json", "whitelists")
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
-    PORT = 5000
+    PORT = 
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
     WORKERS = (
-        8  # Number of subthreads to use. Set as number of threads your processor uses
+        1# Number of subthreads to use. Set as number of threads your processor uses
     )
     BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
-    CASH_API_KEY = (
-        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
+    CASH_API_KEY = (STMXGD0W45Q4ZW5B
+        ""  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
+    TIME_API_KEY = "TIE6CCG34VR5"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
         "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
-    AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    AI_API_KEY = "f98e9b8eae9823626e30996f1f25f0c6e391fb2197b3b7c419f6a40f5ffb56b449fc14b3176a25ea8916f3bc68be57a0f59e08d8085661eb5fb68ed2fee65a93"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
 
